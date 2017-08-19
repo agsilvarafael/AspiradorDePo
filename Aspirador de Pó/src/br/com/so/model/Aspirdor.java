199a329d3abd5d26f7df2493cf290bfab78d4b05
JeronimoHermano <jh.drakos@gmail.com>
2017-08-19 18:25:56 -0300

    Início da GUI.

--- Aspirador de Pó/src/br/com/so/model/Aspirdor.java
+++ Aspirador de Pó/src/br/com/so/model/Aspirdor.java
@@ -1,33 +1,40 @@
 package br.com.so.model;
 
+import br.com.so.util.Timer;
+
 public class Aspirdor {
 
-	private final Double bateriaDuracaoTotal;//Duração em Segundos
-	private Double bateriaDuracaoRestante = 0.0;
+	private final Integer bateriaDuracaoTotalTicks;//Duração em Segundos
+	private Integer bateriaDuracaoRestanteTicks = 0;
 	private final Integer filtroCapacidadeTotal;
 	private Integer filtroQtdAtual = 0;
 	private Integer posX, posY;
+	private Integer limpezaNTicks;
+//	private String urlImage = ;
 
 	/*
 	 * Leva 12 segundos para encher (6 áreas - 2s cada)
 	 * Demora 3s para esvaziar quando está cheio por completo
 	 * 1/2 s para esvaziar cada unidade de sujeira [mas para esvaziar é td ou nada]
 	*/
-	public Aspirdor(Double bateriaDuracaoTotal, Integer filtroCapacidadeTotal) {
-		this.bateriaDuracaoTotal = bateriaDuracaoTotal;
+	public Aspirdor(Integer capacidadeMovimento, Double tempoLimpezaQuadradoSeg,
+			Integer filtroCapacidadeTotal, Double tempoLimpezaFiltro) 
+	{
+		//
+		this.bateriaDuracaoTotalTicks = (int) (capacidadeMovimento + tempoLimpezaQuadradoSeg/Timer.tick);
 		this.filtroCapacidadeTotal = filtroCapacidadeTotal;
 		//a posição inicial é a área de carga/esvaziamento
 		this.posX = 0;
 		this.posY = 5;
 	}
 	
-	public void mover(Double qtd){
-		bateriaDuracaoRestante -= qtd;
+	public void mover(Integer qtd){
+		bateriaDuracaoRestanteTicks -= qtd;
 	}
 	//Retorna TRUE enquanto estiver carregando
 	public Boolean carregar(Double qtd){
 		bateriaDuracaoRestante += qtd;
-		return (bateriaDuracaoTotal>bateriaDuracaoRestante);
+		return (bateriaDuracaoTotalTicks>bateriaDuracaoRestante);
 	}
 	//Retorna TRUE enquanto possuir capacidade
 	public Boolean Limpar(){
@@ -37,16 +44,16 @@ public class Aspirdor {
 	
 	
 	
-	public Double getBateiraDuracaoRestante() {
-		return bateriaDuracaoRestante;
+	public Integer getBateiraDuracaoRestanteTicks() {
+		return bateriaDuracaoRestanteTicks;
 	}
 
-	public void setBateiraDuracaoRestante(Double bateriaDuracaoRestante) {
-		this.bateriaDuracaoRestante = bateriaDuracaoRestante;
+	public void setBateiraDuracaoRestante(Integer bateriaDuracaoRestanteTicks) {
+		this.bateriaDuracaoRestanteTicks = bateriaDuracaoRestanteTicks;
 	}
 
-	public Double getBateriaDuracaoTotal() {
-		return bateriaDuracaoTotal;
+	public Integer getBateriaDuracaoTotalTicks() {
+		return bateriaDuracaoTotalTicks;
 	}
 	
 	
--- Aspirador de Pó/src/br/com/so/view/Frame.java
+++ Aspirador de Pó/src/br/com/so/view/Frame.java
@@ -1,11 +1,48 @@
 package br.com.so.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import java.awt.Label;
+import java.awt.Font;
+import java.awt.Panel;
+import javax.swing.JRadioButton;
 
-public class Frame extends JFrame {
-
-	public Frame() {
-		//TODO
+public class Frame extends JFrame{
+	
+	public static void main(String[] args) {
+		new Frame();
 	}
 	
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Aspirador de p\u00F3 V1.0");
+		setSize(800, 630);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		setLocationRelativeTo(null);
+		setResizable(false);
+		
+		JPanel pContent = new JPanel();
+		pContent.setBackground(Color.WHITE);
+		pContent.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(pContent);
+		pContent.setLayout(null);
+		
+		JPanel panel = new PainelSala();
+		panel.setLocation(0, 0);;
+		pContent.add(panel);
+		
+		
+		
+		this.setVisible(true);
+	}
 }
